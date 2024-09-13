4cb566884110209d2022510685f7a2dd302aa35b
TheIceNarc <mattreiley12@gmail.com>
2024-09-13 13:31:58 -0700

    Vercel Changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:4000', // Replace with your frontend's origin
+    origin: 'https://inventory-management-system-front.vercel.app/', // Replace with your frontend's origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
