5960ce29ac54d4ddbb539b75d1d57cca727c355e
TheIceNarc <mattreiley12@gmail.com>
2024-09-17 12:44:04 -0700

    Vercel FrontEnd

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,4 @@
+// src/app/login/page.tsx
 'use client';
 
 import { useState } from 'react';
@@ -5,63 +6,36 @@ import axios from 'axios';
 import styles from './LoginPage.module.css';
 import Cookie from 'js-cookie';
 import jwt from 'jsonwebtoken';
-import { useRouter } from 'next/router';
-
-// Define the shape of the decoded JWT token
-interface DecodedToken {
-  role: string;
-  exp: number; // Token expiration time
-}
 
 const LoginPage = () => {
-  // State hooks for form inputs and error handling
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState('');
 
-  // Hook for programmatic navigation
-  const router = useRouter();
-
-  // Handle form submission
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      // Make a POST request to the login endpoint
       const response = await axios.post('https://inventorymanagementsystem-kpq9.onrender.com/auth/login', { username, password });
-      const token = response.data.token.access_token;
+      const token  = response.data.token.access_token;
+      const decodedToken = jwt.decode(token) as { role: string };
 
-      // Decode the token to get user role and expiration time
-      const decodedToken = jwt.decode(token) as DecodedToken;
-
-      // Check if the token has expired
-      if (decodedToken.exp < Date.now() / 1000) {
-        throw new Error('Token is expired');
-      }
-
-      // Store the token in cookies for authenticated requests
-      Cookie.set('authToken', token);
-
-      // Redirect user based on their role
-      if (decodedToken.role === 'admin') {
-        router.push('/admin'); // Redirect admin to admin dashboard
+      Cookie.set('authToken', token); 
+      // Redirect or show success message
+      if(decodedToken.role === "admin") {
+        window.location.href = "/admin"
       } else {
-        router.push('/'); // Redirect regular user to home
+        window.location.href = '/'; // Example redirect
       }
-
+      
     } catch (err) {
-      // Handle errors and display a message
-      console.error('Login error:', err);
       setError('Invalid username or password');
     }
   };
-
-  return (
+return(
     <div className={styles.container}>
       <div className={styles.loginCard}>
         <h2 className={styles.title}>Login</h2>
-        {/* Display error message if any */}
         {error && <p className={styles.error}>{error}</p>}
-        {/* Login form */}
         <form onSubmit={handleLogin}>
           <div className={styles.inputGroup}>
             <label htmlFor="username">Username</label>
