517adccb92b3c170b9c520972494886fe171e465
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-06-25 13:20:56 -0700

    cors changes
    changed cors

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -24,6 +24,7 @@ const RegisterPage: React.FC = () => {
     }
 
     try {
+      
       // Send POST request to register a new user
       await axios.post(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/users/register`, { username, password });
       
