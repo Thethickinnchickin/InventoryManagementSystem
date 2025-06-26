7dbba55b9c8f59df904324c14149d347e2184d9b
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-06-26 16:18:30 -0700

    Creating Oder Fix
    Fixing adding the total amount of the order based on items price and quantities

--- frontend/src/app/admin/orders/page.tsx
+++ frontend/src/app/admin/orders/page.tsx
@@ -173,6 +173,11 @@ const OrdersPage = () => {
           { headers: { Authorization: `Bearer ${token}` } }
         );
       } else {
+        let total = 0;
+        for(const item of orderForm.items) {
+          total += (item.price * item.quantity); 
+        }
+        orderForm.totalAmount = total;
         await axios.post(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/orders`, orderForm, {
           headers: { Authorization: `Bearer ${token}` },
         });
