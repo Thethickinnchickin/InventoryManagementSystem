71c4c3c9fa663059e04304d8416f9bf2d851590a
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-06-26 15:59:17 -0700

    testing
    testing

--- frontend/src/app/admin/orders/page.tsx
+++ frontend/src/app/admin/orders/page.tsx
@@ -118,7 +118,7 @@ const OrdersPage = () => {
       const selectedProduct = products.find(
         (product) => product.id === parseInt(value, 10)
       );
-      console.log(product)
+      console.log(selectedProduct);
       if (selectedProduct) {
         updatedItems[index] = {
           ...updatedItems[index],
